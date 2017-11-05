a040d091961834ed3ea5446e4ee343e03225a0e6
Alejok28 <alejo_k28@hotmail.com>
2017-11-05 16:16:33 -0500

    Create PATCH /produtcs/:id route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/products/new', to: 'products#new', as: 'new_product'
   get 'products/:id', to: 'products#show', as: 'product'
   get 'products/:id/edit', to: 'products#edit'
+  patch '/products/:id', to: 'products#update'
 
   post '/products', to: 'products#create'
 end
