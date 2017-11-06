60e01c3187ac22e2039a679a02135d4b4532773f
Alejok28 <alejo_k28@hotmail.com>
2017-11-06 11:26:28 -0500

    Replace the routes for resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get '/products', to: 'products#index'
-  get '/products/new', to: 'products#new', as: 'new_product'
-  get 'products/:id', to: 'products#show', as: 'product'
-  get 'products/:id/edit', to: 'products#edit', as: 'edit_product'
-  patch '/products/:id', to: 'products#update'
-  delete '/products/:id' , to: 'products#destroy'
+  # get '/products', to: 'products#index'
+  # get '/products/new', to: 'products#new', as: 'new_product'
+  # get 'products/:id', to: 'products#show', as: 'product'
+  # get 'products/:id/edit', to: 'products#edit', as: 'edit_product'
+  # patch '/products/:id', to: 'products#update'
+  # delete '/products/:id' , to: 'products#destroy'
+  # post '/products', to: 'products#create'
+  resource :products
 
-  post '/products', to: 'products#create'
+  #solo crea la ruta que contenga la acción index
+  # resource :products, only: [:index]
+
+  #solo crea la ruta que contenga la acción index
+  # resource :products, only: [:index]
 end
